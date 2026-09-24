c47777508167015c466a35d1e9afb789f8c68c28
NexusAman <amankumarkeshri1@gmail.com>
2026-09-24 17:36:59 +0530

    client setup completed

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
