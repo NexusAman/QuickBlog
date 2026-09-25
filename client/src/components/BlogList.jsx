159f64a887b7db2915b2e627de4945945c4298ef
NexusAman <amankumarkeshri1@gmail.com>
2026-09-25 13:37:18 +0530

    feat: add blog cards

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { blogCategories } from "../assets/assets.js";
+import { blog_data, blogCategories } from "../assets/assets.js";
 import { motion } from "motion/react";
+import BlogCard from "./BlogCard";
 
 const BlogList = () => {
   const [menu, setMenu] = useState("All");
@@ -15,12 +16,22 @@ const BlogList = () => {
             >
               {item}
               {menu === item && (
-                <motion.div layoutId="underline" 
-                transition={{type: 'spring', stiffness: 500, damping: 30}} className="absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full"></motion.div>
+                <motion.div
+                  layoutId="underline"
+                  transition={{ type: "spring", stiffness: 500, damping: 30 }}
+                  className="absolute left-0 right-0 top-0 h-7 -z-1 bg-primary rounded-full"
+                ></motion.div>
               )}
             </button>
           </div>
         ))}
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-24 mx-8 sm:mx-16 xl:mx-40">
+        {blog_data
+          .filter((blog) => (menu === "All" ? true : blog.category === menu))
+          .map((blog) => (
+            <BlogCard key={blog._id} blog={blog} />
+          ))}
       </div>
     </div>
   );
